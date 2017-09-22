f07f141f662bde956a23579cff25f1c18b15528e
Влад Купряков <kupryakov@MacBook-Air-Vlad.local>
2017-09-22 12:15:29 +0300

    Initial Commit

---
+++ 5_word/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  5_word
+//
+//  Created by Влад Купряков on 22.09.17.
+//  Copyright © 2017 Влад Купряков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
